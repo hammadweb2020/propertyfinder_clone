2c1a04b6864e2317efa835c033021d95c69f538c
hammadweb2020 <78916257+hammadweb2020@users.noreply.github.com>
2021-12-03 18:23:57 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@ module.exports = {
   resolve:{
       extensions:['.js','.jsx']
    },
+    eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   reactStrictMode: true,
   images:{
     domains:['bayut-production.s3.eu-central-1.amazonaws.com']
